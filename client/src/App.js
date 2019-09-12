e05fb22bb0b286667352c2293fcfb72718a3fabc
vale314 <electrovale314@hotmail.com>
2019-09-12 01:00:45 -0500

    Socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home.js';
+import Home from './components/pages/home.js';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
